af75e5b969a2411826c43b141895cc0b4102ce33
Dani Bassett <dani@danithetallgirl.com>
2020-07-09 02:56:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,11 +8,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array << 
+  create_an_array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  create_an_array.unshift("Olivia")
+  create_an_array.unshift ("")
 end
 
 def remove_element_from_end_of_array(array)
