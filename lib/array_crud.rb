9a062501e8914c4de47a51065b34f646495b26f6
Dani Bassett <dani@danithetallgirl.com>
2020-07-09 02:57:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  create_an_array.unshift ("")
+  create_an_array.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
@@ -24,7 +24,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  create_an_array.index("Dani")
+  create_an_array.index()
 end
 
 def retrieve_first_element_from_array(array)
